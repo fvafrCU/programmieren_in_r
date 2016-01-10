eebd7ac0d1cb1c760670a4aeb1eb2c3f88e090bd
fvafrCU <dominik.cullmann@forst.bwl.de>
2016-01-10 21:51:00 +0100

    cleanup stale files
    readline() works in interactive mode only.

--- examples/guess_my_name.r
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/Rscript --vanilla
-my_name  <- "Dominik Cullmann"
-guess <- readline(prompt = "Guess my name: ")
-if (guess == my_name) {
-    print("You win!")
-} else {
-    print("You loose!")
-}
