196274b6e912dfab8d2fe7a885d039d4c7b5b9e5
fvafrCU <dominik.cullmann@forst.bwl.de>
2016-01-12 12:25:36 +0100

    make update_directory_listings.sh run via make.sh

--- make.sh
+++ make.sh
@@ -6,6 +6,7 @@ find src/ -type f -exec chmod 644 {} \;
 find data/ -type f -exec chmod 644 {} \;
 find answers/ -type f -exec chmod 644 {} \;
 find examples/ -type f -exec chmod 644 {} \;
+./bin/update_directory_listings.sh
 
 grep -v "\[.*\](.*)" index.rmd | wc -L
 grep -v "\[.*\](.*)" index.rmd | awk '{ if (length($0) > max) {max = 80; maxline = $0} } END { print maxline }'
