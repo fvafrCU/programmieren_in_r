e372aefee7abecc7c828e04120410a1d334cb3c7
fvafrCU <dominik.cullmann@forst.bwl.de>
2016-01-13 17:11:35 +0100

    class held, minor modifications

--- answers/readline_test.r
+++ answers/readline_test.r
@@ -2,4 +2,4 @@ my_name  <- "Dominik Cullmann"
 guess <- readline(prompt = "Guess my name: ")
 # "identical(my_name, guess)" is better than "guess == my_name" but harder to
 # read, so I stick with the inferior "==".
-guess == my_name
+print(guess == my_name)
--- index.R
+++ index.R
@@ -234,39 +234,40 @@ for (directory in directories) {
 }
 
 
+not_so_temporary_directory <- file.path(dirname(tempdir()) , "R_intro")
+dir.create(not_so_temporary_directory, showWarnings = FALSE)
 input_directory <- file.path(not_so_temporary_directory, "data", "input")
 dir.create(input_directory, recursive = TRUE)
-if (Sys.info()[["nodename"]] %in% c("foobar", "h5")) {
-    # local machines only
-    file.copy(from = "data/input", to = dirname(input_directory),
-              recursive = TRUE)
-
-} else {
-    dependencies <- c("XML", "RCurl", "httr")
-    for (dependency in dependencies) {
-        if (! require(dependency, character.only = TRUE)) {
-            install.packages(dependency)
-        }
-        library(dependency, character.only = TRUE)
+dependencies <- c("XML", "RCurl", "httr")
+for (dependency in dependencies) {
+    if (! require(dependency, character.only = TRUE)) {
+        install.packages(dependency)
     }
-    input_url <- "http://10.33.50.43/R_Intro/data/input/"
-    proxy <- Sys.getenv("http_proxy")
-    Sys.setenv("http_proxy" = "")
-    content <- getURLContent(input_url, verbose = TRUE)
-    files <- readHTMLTable(content, skip.rows = 1, 
-                           stringsAsFactors = FALSE)[[1]][["Name"]]
-    for (file in files) {
-        file_url <- file.path(input_url, file)
-        file_path <- file.path(input_directory, file)
-        if (Sys.info()["sysname"] == "Windows") {
-            download.file(file_url, file_path, method = "wininet", mode = "wb")  
-        } else {
-            download.file(file_url, file_path, method = "curl")  
-        }
-    }
-
-    Sys.setenv("http_proxy" = proxy)
+    library(dependency, character.only = TRUE)
 }
+input_url <- "http://fvafrcu.github.io/programmieren_in_r/data/input/"
+proxy <- Sys.getenv("http_proxy")
+Sys.setenv("http_proxy" = "")
+content <- getURLContent(input_url, verbose = TRUE)
+hrefs <- grep("/", 
+              grep("href", 
+                   strsplit(as.character(content), "<")[[1]]
+                   , value = TRUE
+                   )
+              , value = TRUE, invert = TRUE
+              )
+files  <- sapply( strsplit(hrefs, "\\\"") ,"[[", 2)  
+for (file in files) {
+    file_url <- file.path(input_url, file)
+    file_path <- file.path(input_directory, file)
+    if (Sys.info()["sysname"] == "Windows") {
+        download.file(file_url, file_path, method = "wininet", mode = "wb")  
+    } else {
+        download.file(file_url, file_path, method = "curl")  
+    }
+}
+
+Sys.setenv("http_proxy" = proxy)
 
 
 species_shares_1987 <- read.csv2(file.path(input_directory, 
