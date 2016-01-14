f8dfc3e020e31efbb21eef38e746fdf61b925138
fvafrCU <dominik.cullmann@forst.bwl.de>
2016-01-14 18:26:47 +0100

    run make

--- index.R
+++ index.R
@@ -367,6 +367,37 @@ v3[c("FI", "DGL")]
 species_shares_1987[["species_group_label"]] 
 mode(species_shares_1987[["species_group_label"]])
 class(species_shares_1987[["species_group_label"]])
+create_personal_directories <- function(file, 
+                                        path = file.path(dirname(tempdir()), 
+                                                         "personal_directories")
+                                        ){
+    #% read the file containing the names
+    lines <- readLines("files/names.txt")
+    #% extract names, order them in FIRSTNAME_LASTNAME
+    non_empty_lines <- lines[which(lines != "")]
+    tmp <- sub("^ *", "", non_empty_lines)
+    names <- sub(" *", "", tmp)
+    last_names <- sapply(strsplit(names, ","), "[", 1)
+    first_names <-  gsub(" ", "_", 
+                         sub(" *$", "", 
+                             sub("^ *", "", 
+                                 sapply(strsplit(names, ","), "[", 2)
+                                 )
+                             )
+                         )
+    directories <- paste(first_names, last_names, sep = "_")
+    #% create path 
+    dir.create(path, showWarnings = FALSE, recursive = TRUE)
+    #% create those personal directories
+    for (directory in directories) {
+        path <- file.path(path, directory)
+        dir.create(path)
+    }
+}
+
+
+create_personal_directories("files/names.txt")
+
 # we need to convert the factor's values to character!
 (abbreviation <- as.character(species_shares_1987["1", "species_group_label"]))
 (text  <- 
