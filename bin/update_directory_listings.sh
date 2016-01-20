b3868abd8c950909420e85fd7a53ff69e1e603b9
fvafrCU <dominik.cullmann@forst.bwl.de>
2016-01-20 20:02:00 +0100

    make index.html for all subdirectories

--- bin/update_directory_listings.sh
+++ bin/update_directory_listings.sh
@@ -2,7 +2,7 @@
 # adapted from http://blog.kaltepoth.de/
 # github pages accept static html only, so we need to create static directory
 # listings for subdirectories.
-for DIR in src data data/input; do
+for DIR in $(find .  -mindepth 1 -type d ! -path "./.git*" -print); do
   (
     echo  "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
     ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | \
