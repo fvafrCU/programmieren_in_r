2c3ec507cba39351ec1c7dad8013b0fe20830be9
fvafrCU <fvafrcu@arcor.de>
2017-11-16 11:11:25 +0100

    minor corrections

--- index.R
+++ index.R
@@ -213,9 +213,9 @@ cat("My first readme file.", file = not_so_temporary_file, sep = "\n")
 
 ## ... some days later
 if (file.exists(not_so_temporary_file)) {
-    file.rename(not_so_temporary_file, paste0(not_so_temporary_file, "1"))
+    file.rename(not_so_temporary_file, paste0(not_so_temporary_file, ".bak"))
 }
-cat("This is a directory created by 'Programmieren in R' .",
+cat("This is a directory created by 'Programmieren in R'.",
     file = not_so_temporary_file, sep = "\n")
 
 
